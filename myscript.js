1da1fc7c5f9f5ccc3cb3998eb9ff5aa68c314d5a
Sanjay Sekar Samuel <36979615+sanjaysamuels@users.noreply.github.com>
2022-09-28 23:15:59 -0400

    "first commit"

---
+++ myscript.js
@@ -0,0 +1,9 @@
+var navigationLink = document.getElementById("navigationLinks");
+
+        function showMenu(){
+            navigationLink.style.right = "0";
+        }
+
+        function hideMenu(){
+            navigationLink.style.right = "-200px";
+        }
